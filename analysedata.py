af41ef92ce6b429e0756d7199568aa227b0b1fd3
slovanos <apolos@gmail.com>
2024-10-30 14:31:21 +0100

    reformat cli output for friendly markdown

--- analysedata.py
+++ analysedata.py
@@ -24,8 +24,9 @@ def preprocess_data(
     df = pd.read_csv(file_path, usecols=[0, 1])
     if verbose:
         print("Initial Dataset Info:")
-        print(df.info(verbose=False))
-        print(df.describe())
+        df.info(verbose=False)
+        print()
+        print(df.describe().to_markdown())
 
         # Check for empty values and duplicates
         empty_values = df[df.isna().any(axis=1)]
@@ -44,10 +45,11 @@ def preprocess_data(
     df.drop_duplicates(inplace=True, ignore_index=True)
 
     if verbose:
-        print("-" * 80)
+        print("\n" + "-" * 80)
         print("\nPreprocessed Dataset Info:")
-        print(df.info(verbose=False))
-        print(df.describe())
+        df.info(verbose=False)
+        print()
+        print(df.describe().to_markdown())
 
     if save_file:
         output_file = get_new_file_path(suffix="_preprocessed")
@@ -57,7 +59,9 @@ def preprocess_data(
     return df
 
 
-def calculate_complexity_scores(df: pd.DataFrame, verbose: bool = True, tophard: bool = False) -> pd.DataFrame:
+def calculate_complexity_scores(
+    df: pd.DataFrame, verbose: bool = True, tophard: bool = False
+) -> pd.DataFrame:
     """
     Calculate Flesch Reading Ease (FRE) score and Wiener Sachtextformel (WSTF) score for a given DataFrame.
 
@@ -85,31 +89,37 @@ def calculate_complexity_scores(df: pd.DataFrame, verbose: bool = True, tophard:
         average_leichte_sprache_wstf = df['Leichte Sprache WSTF Score'].mean().round(2)
 
         # Print scores and averages
-        print("=" * 80)
+        print("\n" + "=" * 80)
         print("\nText Complexity Scores")
         print("\nFlesch Reading Ease Score (low = hard, high = easy):")
         print("Average Original FRE Score:", average_original)
         print("Average Leichte Sprache FRE Score:", average_leichte_sprache)
-        print("-" * 80)
-        print("Wiener Sachtextformel: (min: 4 = easy, max: ~15 = hard)")
+        print("\n" + "-" * 80)
+        print("\nWiener Sachtextformel: (min: 4 = easy, max: ~15 = hard)")
         print("Average Original WSTF Score:", round(average_original_wstf, 2))
         print("Average Leichte Sprache WSTF Score:", round(average_leichte_sprache_wstf, 2))
 
     if tophard:
-        print("-" * 80)
         top_n = min(10, round(df.shape[0] * 0.1))
-        print("=" * 80)
+        print("\n" + "-" * 80)
         print(f"\nTop {top_n} hardest samples according Flesch Reading Ease Score:\n")
         fre_top = df.sort_values(by='Original FRE Score', ascending=True)[:top_n]
         print(
-            fre_top[["Original", "Original FRE Score", "Leichte Sprache", "Leichte Sprache FRE Score"]].to_markdown()
+            fre_top[
+                ["Original", "Original FRE Score", "Leichte Sprache", "Leichte Sprache FRE Score"]
+            ].to_markdown()
         )
 
         print(f"\nTop {top_n} hardest samples according Wiener Sachtextformel Score:\n")
         wstf_top = df.sort_values(by='Original WSTF Score', ascending=False)[:top_n]
         print(
             wstf_top[
-                ["Original", "Original WSTF Score", "Leichte Sprache", "Leichte Sprache WSTF Score"]
+                [
+                    "Original",
+                    "Original WSTF Score",
+                    "Leichte Sprache",
+                    "Leichte Sprache WSTF Score",
+                ]
             ].to_markdown()
         )
 
@@ -188,15 +198,15 @@ def main(
     df = calculate_complexity_scores(df, tophard=True)
 
     if verbose:
-        print("=" * 80)
-        print("\nAnalysed Data Stats:")
+        print("\n" + "="*80)
+        print("\nAnalysed Data Stats:\n")
         print(df.describe().to_markdown())
 
     output_filename = None
     if save_file:
         output_filename = get_new_file_path(file_path, suffix="_analysed")
         df.to_csv(output_filename, index=False, encoding="utf-8")
-        print(f"Saved analysed dataset to {output_filename}")
+        print(f"\nSaved analysed dataset to {output_filename}")
 
     if plot:
         show_graph = True if df.shape[0] < 100 else False
