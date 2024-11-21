3d742204aa972f7520ace9546fef5aa4db9b18ef
slovanos <apolos@gmail.com>
2024-11-21 13:27:29 +0100

    update installation script with interactive options

--- bin/one-script-to-install-them-all.sh
+++ bin/one-script-to-install-them-all.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 echo 'Installing/Updating ollama...' && \
 curl -fsSL https://ollama.ai/install.sh | sh && \
-echo 'Creating virtual environment...' && \
-python3 -m venv .myvenv && \
-echo 'Activating virtual environment...' && \
-source .myvenv/bin/activate && \
+read -p "Install the Leichte Sprache tool in a virtual environment? (Type 'y' to agree, or leave blank to skip. Press Enter):" VENV_CHOICE
+if [[ $VENV_CHOICE == [yY] ]]; then
+	echo 'Creating virtual environment...' && \
+	python3 -m venv .myvenv && \
+	echo 'Activating virtual environment...' && \
+	source .myvenv/bin/activate
+fi
 echo 'Installing requirements...' && \
 pip3 install -r requirements.txt
+read -p "Install the additional tools? ('y' to agree, or press Enter to skip):" TOOLS_CHOICE
+if [[ $TOOLS_CHOICE == [yY] ]]; then
+	echo 'Installing additional tools...' && \
+	pip3 install -r requirements_extended.txt
+fi
 echo 'Pulling ollama models...' && \
 echo -e '\nPulling llama 3.1 ...' && \
 ollama pull llama3.1 || echo 'choose and download a model with $ ollama pull <your_model_of_choice>' && \
